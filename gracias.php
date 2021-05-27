c4907d3a0a610c7c5bb8f34a049e55c7a387b5cb
villa1013 <williamvillalba13@gmail.com>
2021-05-27 18:58:39 -0500

    Resposive fix

--- gracias.php
+++ gracias.php
@@ -77,12 +77,14 @@
         <div class="homepage-content">
             <div class="container">
                 <div class="row">
-                    <div class="col-lg-6 col-md-4 col-sm-12 relative">
+                    <div class="col-lg-12 col-md-12 col-sm-12 relative min-heigh-thank">
                         <!-- Info -->
-                        <div class="title-mack">
-                            <h3>¡ÚLTIMAS UNIDADES</h3>
-                            <h1>MACK ANTHEM 2020</h1>
-                            <h3>CON PRECIO EN PESOS!</h3>
+                        <div class="title-mack thank">
+                         Y CV  
+                            <h3>¡TUS DATOS</h3>
+                            <h1>HAN SIDO ENVIADOS CON ÉXITO!</h1>
+                            <h3>MUY PRONTO NOS COMUNICAREMOS CON USTED!</h3>
+                            <a href="index.php" class="btn btn-primary">Volver al inicio</a>
                         </div>
                         <div class="mack-contact">
                             <img src="assets/images/iconos-seguridad.svg" />
@@ -94,170 +96,6 @@
                             </div>
                         </div>
                         <!--/ Info -->
-                    </div>
-                    <div class="col-lg-6 col-md-8 col-sm-12">
-                        <!-- Form Cotizar Mack -->
-                        <section class="container_form_mack">
-                            <div class="title-subtitle text-right">
-                                <h3>CONTÁCTANOS</h3>
-                                <p>NUESTROS ASESORES SE CONTACTARÁN CONTIGO</p>
-                            </div>
-                            <div class="bg_form">
-                                <h5 class="d-flex mb-2"><img src="assets/images/icon-contact.svg" /> TU INFOMORMACIÓN</h5>
-                                <form id="FormCotizar" action="enviarcorreo.php" method="post" novalidate="novalidate">
-                                    <div class="row">
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Nombre(s)</label>
-                                                <input type="text" name="nombres" id="nombres" size="40" class="form-control" aria-required="true" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Apellidos</label>
-                                                <input type="text" name="apellido" id="apellido" size="40" class="form-control" aria-required="true" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Celular</label>
-                                                <input type="number" name="celular" id="celular" size="40" class="form-control" aria-required="true" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Correo</label>
-                                                <input type="email" name="email" id="email" size="40" class="form-control" aria-required="true" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Cédula</label>
-                                                <input type="number" name="cedula" id="cedula" size="40" class="form-control" aria-required="true" />
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-6">
-                                            <div class="form-group">
-                                                <label>Ciudad</label>
-                                                <select name="ciudad" id="ciudad" class="form-control" aria-required="true">
-                                                    <option value="">Seleccionar</option>
-                                                    <option value="Aguachica Cesar || 20011">Aguachica Cesar</option>
-                                                    <option value="Apartadó || 5045">Apartadó</option>
-                                                    <option value="Arauca || 81001">Arauca</option>
-                                                    <option value="Armenia || 5059">Armenia </option>
-                                                    <option value="Barrancabermeja || 68081">Barrancabermeja</option>
-                                                    <option value="Barranquilla || 8001">Barranquilla</option>
-                                                    <option value="Bello || 5088">Bello </option>
-                                                    <option value="Bogotá || 11001">Bogotá</option>
-                                                    <option value="Bucaramanga || 68001">Bucaramanga </option>
-                                                    <option value="Buenaventura || 76109">Buenaventura</option>
-                                                    <option value="Buga || 76111">Buga</option>
-                                                    <option value="Cali || 76001">Cali</option>
-                                                    <option value="Cartago || 76147">Cartago</option>
-                                                    <option value="Cartagena || 13001">Cartagena</option>
-                                                    <option value="Caucasia || 5154">Caucasia</option>
-                                                    <option value="Cereté || 23162">Cereté </option>
-                                                    <option value="Chia || 25175">Chia</option>
-                                                    <option value="Ciénaga || 47189">Ciénaga </option>
-                                                    <option value="Cúcuta || 54001">Cúcuta</option>
-                                                    <option value="Dosquebradas || 66170">Dosquebradas </option>
-                                                    <option value="Duitama || 15238">Duitama</option>
-                                                    <option value="Envigado || 5266">Envigado</option>
-                                                    <option value="Facatativá || 25269">Facatativá </option>
-                                                    <option value="Florencia || 18001">Florencia</option>
-                                                    <option value="Floridablanca || 68276">Floridablanca</option>
-                                                    <option value="Fusagasugá || 25290">Fusagasugá </option>
-                                                    <option value="Girardot || 25307">Girardot</option>
-                                                    <option value="Girón  || 68307">Girón </option>
-                                                    <option value="Ibagué || 73001">Ibagué</option>
-                                                    <option value="Ipiales || 52356">Ipiales</option>
-                                                    <option value="Itagüí || 5360">Itagüí</option>
-                                                    <option value="Jamundí || 76364">Jamundí</option>
-                                                    <option value="Lorica || 23417">Lorica</option>
-                                                    <option value="Los Patios || 54405">Los Patios</option>
-                                                    <option value="Magangué || 13430">Magangué</option>
-                                                    <option value="Maicao || 44430">Maicao</option>
-                                                    <option value="Malambo || 8433">Malambo</option>
-                                                    <option value="Manizales || 17001">Manizales </option>
-                                                    <option value="Medellín || 5001">Medellín </option>
-                                                    <option value="Melgar || 73449">Melgar </option>
-                                                    <option value="Montería || 23001">Montería</option>
-                                                    <option value="Neiva || 41001">Neiva </option>
-                                                    <option value="Ocaña || 54498">Ocaña</option>
-                                                    <option value="Paipa || 15516">Paipa</option>
-                                                    <option value="Palmira || 76520">Palmira</option>
-                                                    <option value="Pamplona || 54518">Pamplona</option>
-                                                    <option value="Pasto || 52001">Pasto</option>
-                                                    <option value="Pereira || 66001">Pereira</option>
-                                                    <option value="Piedecuesta || 68547">Piedecuesta</option>
-                                                    <option value="Pitalito || 41551">Pitalito</option>
-                                                    <option value="Popayán || 19001">Popayán </option>
-                                                    <option value="Quibdó || 27001">Quibdó </option>
-                                                    <option value="Riohacha || 44001">Riohacha</option>
-                                                    <option value="Rionegro || 5615">Rionegro</option>
-                                                    <option value="Sabanalarga || 5628">Sabanalarga</option>
-                                                    <option value="Sahagún || 23660">Sahagún </option>
-                                                    <option value="San Andrés || 5647">San Andrés</option>
-                                                    <option value="Santa Marta || 47001">Santa Marta </option>
-                                                    <option value="Sincelejo || 70001">Sincelejo</option>
-                                                    <option value="Soacha || 25754">Soacha </option>
-                                                    <option value="Sogamoso || 15759">Sogamoso</option>
-                                                    <option value="Soledad || 8758">Soledad </option>
-                                                    <option value="Tibú || 54810">Tibú </option>
-                                                    <option value="Tuluá || 76834">Tuluá</option>
-                                                    <option value="Tumaco || 52835">Tumaco </option>
-                                                    <option value="Tunja || 15001">Tunja </option>
-                                                    <option value="Turbo || 5837">Turbo</option>
-                                                    <option value="Valledupar || 20001">Valledupar</option>
-                                                    <option value="Villa || 54871">Villa</option>
-                                                    <option value="Villa del Rosario || 54874">Villa del Rosario</option>
-                                                    <option value="Villavicencio || 50001">Villavicencio</option>
-                                                    <option value="Yopal || 85001">Yopal </option>
-                                                    <option value="Yumbo || 76892">Yumbo</option>
-                                                    <option value="Zipaquirá || 25899">Zipaquirá</option>
-                                                </select>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-12">
-                                            <h6>AUTORIZACIÓN PARA EL USO Y TRATAMIENTO DE DATOS E INFORMACIÓN PERSONAL</h6>
-                                            <div class="form-check checkbox">
-                                                <label class="form-check-label container-checkbox">
-                                                    Declaro que he sido informado por el grupo empresarial compuesto por las sociedades 
-                                                    <b>Praco Didacol S.A.S, Distribuidora Hino de Colombia S.A.S, Inmobiliaria Inchcape Colombia S.A.S y  Matrase S.A.S,;</b> 
-                                                    en adelante Las Compañías, que serán el Responsable del Tratamiento de mis  Datos Personales, por lo que autorizo de manera previa, libre, voluntaria, inequívoca, expresa e informada a Las Compañías o quien represente sus derechos, también a la sociedad matriz y/o subordinadas en Colombia o en el exterior, y en especial, a cualquiera de las sociedades del grupo INCHCAPE dentro de las que se encuentra Inchcape DDC Colombia S.A.S, e incluyendo mas no limitándose a INCHCAPE PLC, y en general a sus aliados estratégicos y/o distribuidores autorizados, fabricantes de vehículos que representan e importan y a los concesionarios de la red de Las Compañías en Colombia a recolectar, almacenar, usar, circular y suprimir mis datos personales, conforme a la Política de Tratamiento de Datos Personales descrita en el siguiente link www.pracodidacol.com. 
-                                                    <b>Con la presente autorización del tratamiento de mis datos personales se me podrá contactar mediante sms, whatsapp, correo electrónico, dirección de residencia, llamada telefónica y/o cualquier otro medio de comunicación instantáneo o formal por motivos de:  Envío de información relacionada con todo el concepto de experiencia de cualquiera de las marcas que hacen parte de Las Compañías, ya sea para fines de actualización de datos, invitación a eventos, envío de publicidad, fines estadísticos, envío de información de productos y servicios, ofertas, descuentos, información corporativa, auditorias, seguimientos, promociones, encuestas, llamadas de seguimiento, envío de productos a la dirección de residencia, así como envío de información de campañas técnicas de servicio, seguridad o “recall”, de productos financieros y de seguros para la adquisición de bienes y servicios  de cualquiera de las marcas que hacen parte de Las Compañías; de igual forma, esta autorización comprende el mandato expreso para que mi información haga parte de la Base de Datos de cualquiera de las Compañías, así como también pueda ser transferida y/o transmitida a otras compañías externas del grupo con residencia en Colombia o en otros países, incluyendo mas no limitándose a las marcas de los productos que importa, comercializa y distribuyen las Compañías en Colombia.</b> 
-                                                    Como titular de la información soy consciente que tengo el derecho a conocer, actualizar y rectificar mis datos personales, solicitar prueba de la autorización otorgada para su trámite, revocar la autorización y/o solicitar la supresión de mis datos por medio de una solicitud escrita dirigida a: 
-                                                    <a href="mailto:didacol@didacol.com" target="_blank">didacol@didacol.com</a>.
-                                                </label>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-12 d-flex py-3">
-                                            <div class="radioBtn checkAutorizo">
-                                                <input type="radio" id="si" name="terminos" value="on" />
-                                                <label for="si">ACEPTO</label>
-                                            </div>
-                                            <div class="radioBtn checkNoAutorizo">
-                                                <input type="radio" id="no" name="terminos" value="off" />
-                                                <label for="no">NO ACEPTO</label>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-12">
-                                            <div class="checkAutorizoInfo">
-                                                <p>En resumen, autorizo contactarme por cualquier medio para el envío de información relacionada con publicidad, invitación a eventos, envío de productos - promociones y encuestas, llamadas de seguimiento, recalls de cualquiera de las marcas de Las Compañías entre otras finalidades relacionadas en la autorización. Así mismo, autorizo el almacenamiento de mi información.</p>
-                                            </div>
-                                        </div>
-                                        <div class="col-sm-12">
-                                            <button type="submit" class="btn btn-primary block w-full">ENVIAR</button>
-                                        </div>
-                                        <div class="col-sm-12 campaing">
-                                            <h6>FORTALECEMOS EL VÍNCULO ESENCIAL ENTRE EL HOMBRE Y LA MÁQUINA</h6>
-                                            <p>*VÁLIDO DEL 20 DE MAYO AL 30 DE JUNIO DE 2021. MAYOR INFORMACIÓN EN WWW.PRACODIDACOL.COM</p>
-                                        <div>
-                                    </div>
-                                </form>
-                            </div>
-                        </section>
-                        <!-- / Form Cotizar Mack -->
                     </div>
                 </div>
             </div>
